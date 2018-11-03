8be2c4ab63c30ad8b371f8c5de3f379553bcc949
ZeLingNing <nzl199851@163.com>
2018-11-03 20:38:56 +0800

    Update 数据库登陆封装函数/sqlconfig.php

---
+++ 数据库登陆封装函数/sqlconfig.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carzy_Ning
+ * Date: 2018/10/31
+ * Time: 11:04
+ */
+
+
+header("content-type:text/html;charset=utf8");
+
+
+$SqlConfig=  [
+    "host"=>"localhost",
+    "user"=>"root",
+    "pass"=>1998,
+    "port"=>"3306",
+    "db"=>"goods"
+];
+
